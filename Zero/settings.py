2b11cc3f21e836e36d89ebede620e48c4ffa4773
Prashastp25 <86373449+Prashastp25@users.noreply.github.com>
2021-09-01 15:50:21 +0530

    Update settings.py

--- Zero/settings.py
+++ Zero/settings.py
@@ -129,3 +129,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
