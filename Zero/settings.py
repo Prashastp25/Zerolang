d1588a5e6b50f02df27638190448ed6a1d1a47f8
Prashastp25 <86373449+Prashastp25@users.noreply.github.com>
2021-09-01 20:19:31 +0530

    Update settings.py

--- Zero/settings.py
+++ Zero/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'whitenoise.runserver_nostatic'
+     'whitenoise.runserver_nostatic',
+    
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
