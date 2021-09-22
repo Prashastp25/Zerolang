c44350b8066a751f6c8c69a3b0dbb154c94c7c22
Prashastp25 <86373449+Prashastp25@users.noreply.github.com>
2021-09-22 23:52:45 +0530

    Update main.py

--- static/main.py
+++ static/main.py
@@ -1,16 +1,32 @@
-arr_keywords = ['-', 'import', '*', '+']
-arr_desire_wrds = ['minus', 'asd', 'asdd', 'asx']
-temp_store = []
-def main():
-    keywords = input()
-    desire_wrds = input()
-    arr_desire_wrds.append(desire_wrds)
-    arr_keywords.append(keywords)
-    fin = input()
-    temp_store.append(fin)
-    for index in range(0, len(arr_keywords)):
-        temp_store.append(temp_store[-1].replace(arr_desire_wrds[index],arr_keywords[index]))
-    exec(temp_store[-1])
+import mimetypes
+from firebase import firebase
+import requests
+import json
+    config = {
+  "apiKey": "AIzaSyAblsApi4a-W2mlMlSi0P_pM3IAocb6fSg",
+  "authDomain": "zero-ae545.firebaseapp.com",
+  "databaseURL": "https://zero-ae545-default-rtdb.firebaseio.com",
+  "projectId": "zero-ae545",
+  "storageBucket": "zero-ae545.appspot.com",
+  "messagingSenderId": "240760199305",
+  "appId": "1:240760199305:web:684c9d9aa67b2604624e58",
+  "measurementId": "G-0BHGLL4QWY"
+    }
+    db = firebase.FirebaseApplication("https://zero-ae545-default-rtdb.firebaseio.com",None)
+    result = db.get('/zero-ae545/Data','input')
+    req_data = requests.get("https://zero-ae545-default-rtdb.firebaseio.com/Data.json").json()
+    data_key, data_val = zip(*req_data.items())
+    print(req_data)
+    print(result)
+    arr_keywords = list(data_key)
+    arr_desire_wrds = list(data_val)
+    temp_store = []
+    def main():
+        fin = input()
+        temp_store.append(fin)
+        for index in range(0, len(arr_keywords)):
+            temp_store.append(temp_store[-1].replace(arr_desire_wrds[index],arr_keywords[index]))
+        exec(temp_store[-1])
 
-while True:
-    main()
+    while True:
+        main()
